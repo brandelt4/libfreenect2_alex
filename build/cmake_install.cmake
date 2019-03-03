323a025c644e7b17f64874af2a53f639b86d840a
brandelt4 <45299633+brandelt4@users.noreply.github.com>
2019-03-03 16:29:59 +0000

    Initial commit

---
+++ build/cmake_install.cmake
@@ -0,0 +1,90 @@
+# Install script for directory: C:/libfreenect2_altered
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/libfreenect2_altered/build/install")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/libfreenect2_altered/build/lib/Debug/freenect2d.lib")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/libfreenect2_altered/build/lib/Release/freenect2.lib")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/libfreenect2_altered/build/lib/MinSizeRel/freenect2.lib")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/libfreenect2_altered/build/lib/RelWithDebInfo/freenect2.lib")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE SHARED_LIBRARY FILES "C:/libfreenect2_altered/build/bin/Debug/freenect2d.dll")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE SHARED_LIBRARY FILES "C:/libfreenect2_altered/build/bin/Release/freenect2.dll")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE SHARED_LIBRARY FILES "C:/libfreenect2_altered/build/bin/MinSizeRel/freenect2.dll")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE SHARED_LIBRARY FILES "C:/libfreenect2_altered/build/bin/RelWithDebInfo/freenect2.dll")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "C:/libfreenect2_altered/include/libfreenect2" REGEX "/[^/]*\\.in$" EXCLUDE)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "C:/libfreenect2_altered/build/libfreenect2")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/freenect2" TYPE FILE FILES "C:/libfreenect2_altered/build/freenect2Config.cmake")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "C:/libfreenect2_altered/build/freenect2.pc")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/libfreenect2_altered/build/examples/cmake_install.cmake")
+
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "C:/libfreenect2_altered/build/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ src/packet_pipeline.cpp
@@ -0,0 +1,132 @@
+/*
+ * This file is part of the OpenKinect Project. http://www.openkinect.org
+ *
+ * Copyright (c) 2014 individual OpenKinect contributors. See the CONTRIB file
+ * for details.
+ *
+ * This code is licensed to you under the terms of the Apache License, version
+ * 2.0, or, at your option, the terms of the GNU General Public License,
+ * version 2.0. See the APACHE20 and GPL2 files for the text of the licenses,
+ * or the following URLs:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * If you redistribute this file in source form, modified or unmodified, you
+ * may:
+ *   1) Leave this header intact and distribute it under the same terms,
+ *      accompanying it with the APACHE20 and GPL20 files, or
+ *   2) Delete the Apache 2.0 clause and accompany it with the GPL2 file, or
+ *   3) Delete the GPL v2 clause and accompany it with the APACHE20 file
+ * In all cases you must keep the copyright notice intact and include a copy
+ * of the CONTRIB file.
+ *
+ * Binary distributions must follow the binary distribution requirements of
+ * either License.
+ */
+
+/** @file packet_pipeline.cpp Packet pipeline implementation. */
+
+#include <libfreenect2/packet_pipeline.h>
+#include <libfreenect2/async_packet_processor.h>
+#include <libfreenect2/data_callback.h>
+#include <libfreenect2/rgb_packet_stream_parser.h>
+#include <libfreenect2/depth_packet_stream_parser.h>
+
+namespace libfreenect2
+{
+
+class PacketPipelineComponents
+{
+public:
+  RgbPacketStreamParser *rgb_parser_;
+  DepthPacketStreamParser *depth_parser_;
+
+  RgbPacketProcessor *rgb_processor_;
+  BaseRgbPacketProcessor *async_rgb_processor_;
+  DepthPacketProcessor *depth_processor_;
+  BaseDepthPacketProcessor *async_depth_processor_;
+
+  ~PacketPipelineComponents();
+  void initialize(RgbPacketProcessor *rgb, DepthPacketProcessor *depth);
+};
+
+void PacketPipelineComponents::initialize(RgbPacketProcessor *rgb, DepthPacketProcessor *depth)
+{
+  rgb_parser_ = new RgbPacketStreamParser();
+  depth_parser_ = new DepthPacketStreamParser();
+
+  rgb_processor_ = rgb;
+  depth_processor_ = depth;
+
+  async_rgb_processor_ = new AsyncPacketProcessor<RgbPacket>(rgb_processor_);
+  async_depth_processor_ = new AsyncPacketProcessor<DepthPacket>(depth_processor_);
+
+  rgb_parser_->setPacketProcessor(async_rgb_processor_);
+  depth_parser_->setPacketProcessor(async_depth_processor_);
+}
+
+PacketPipelineComponents::~PacketPipelineComponents()
+{
+  delete async_rgb_processor_;
+  delete async_depth_processor_;
+  delete rgb_processor_;
+  delete depth_processor_;
+  delete rgb_parser_;
+  delete depth_parser_;
+}
+
+PacketPipeline::PacketPipeline(): comp_(new PacketPipelineComponents()) {}
+
+PacketPipeline::~PacketPipeline()
+{
+  delete comp_;
+}
+
+PacketPipeline::PacketParser *PacketPipeline::getRgbPacketParser() const
+{
+  return comp_->rgb_parser_;
+}
+
+PacketPipeline::PacketParser *PacketPipeline::getIrPacketParser() const
+{
+  return comp_->depth_parser_;
+}
+
+RgbPacketProcessor *PacketPipeline::getRgbPacketProcessor() const
+{
+  return comp_->rgb_processor_;
+}
+
+DepthPacketProcessor *PacketPipeline::getDepthPacketProcessor() const
+{
+  return comp_->depth_processor_;
+}
+
+CpuPacketPipeline::CpuPacketPipeline()
+{ 
+  comp_->initialize(new TurboJpegRgbPacketProcessor(), new CpuDepthPacketProcessor());
+}
+
+CpuPacketPipeline::~CpuPacketPipeline() { }
+
+#ifdef LIBFREENECT2_WITH_OPENGL_SUPPORT
+OpenGLPacketPipeline::OpenGLPacketPipeline(void *parent_opengl_context, bool debug) : parent_opengl_context_(parent_opengl_context), debug_(debug)
+{ 
+  comp_->initialize(new TurboJpegRgbPacketProcessor(), new OpenGLDepthPacketProcessor(parent_opengl_context_, debug_));
+}
+
+OpenGLPacketPipeline::~OpenGLPacketPipeline() { }
+#endif // LIBFREENECT2_WITH_OPENGL_SUPPORT
+
+
+#ifdef LIBFREENECT2_WITH_OPENCL_SUPPORT
+
+OpenCLPacketPipeline::OpenCLPacketPipeline(const int deviceId) : deviceId(deviceId)
+{ 
+  comp_->initialize(new TurboJpegRgbPacketProcessor(), new OpenCLDepthPacketProcessor(deviceId));
+}
+
+OpenCLPacketPipeline::~OpenCLPacketPipeline() { }
+#endif // LIBFREENECT2_WITH_OPENCL_SUPPORT
+
+} /* namespace libfreenect2 */
