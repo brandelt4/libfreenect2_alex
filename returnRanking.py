919b612bf9fcf7449f01a7e097c2d15762810943
Alex <brandelt4@gmail.com>
2019-03-04 16:35:22 +0000

    Default Changelist

--- returnRanking.py
+++ returnRanking.py
@@ -4,21 +4,15 @@
 import main
 import pandas as pd
 import sys
-from train_classifier import impute
 sys.path.append('~/libfreenect2_alex/build')
 
 
 def _calculate(test_vec):
 
-    data = pd.DataFrame
-    # index_ = 0
-    df = pd.DataFrame
+    print("************* i am calculating *************")
 
-    # i = 0
-    # j = 0
 
-    even_list = []  # first frequency
-    # odd_list = []  # second frequency
+    even_list = []  # frequency
     for mat_type_data in test_vec:
         for index, value in enumerate(mat_type_data):
             even_list.append(value)
@@ -26,7 +20,6 @@ def _calculate(test_vec):
 
     # material = training_set[i][j]
     # even_list.append(
-    print(data)
     data = pd.DataFrame([even_list])
 
     # classifiers = give_classifiers()
