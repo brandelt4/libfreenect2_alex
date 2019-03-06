357ec5329149eed4466991bf7eb5f220845d59f7
Alex <brandelt4@gmail.com>
2019-03-06 22:15:22 +0000

    asd

--- arduino_motor/arduino_motor.ino
+++ arduino_motor/arduino_motor.ino
@@ -8,6 +8,9 @@ const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
 
 int StepsRequired;
 
+const int buttonPin = 2;
+
+int buttonState = 0;
 
 Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11);
 
@@ -15,18 +18,28 @@ Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11);
 void setup() {
   // put your setup code here, to run once:
 
+  pinMode(buttonPin, INPUT);
+
 }
 
 void loop() {
 
-  steppermotor.setSpeed(1);
-  StepsRequired = 4;
-  steppermotor.step(StepsRequired);
-  delay(2000);
+  buttonState = digitalRead(buttonPin);
 
-  StepsRequired = STEPS_PER_OUT_REV / 2;
-  steppermotor.setSpeed(100);
-  steppermotor.step(StepsRequired);
-  delay(1000);
+  if (buttonState == HIGH) {
+    steppermotor.setSpeed(1);
+    StepsRequired = 4;
+    steppermotor.step(StepsRequired);
+    delay(2000);
+  
+    StepsRequired = STEPS_PER_OUT_REV / 2;
+    steppermotor.setSpeed(100);
+    steppermotor.step(StepsRequired);
+    delay(1000);
+    }
+
+  else {
+    ;
+    }
 
 }
